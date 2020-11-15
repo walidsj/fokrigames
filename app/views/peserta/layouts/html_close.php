d51c5f0fd6808f1052cdaf4261e51f68966b642c
Moh. Walid Arkham Sani <mohwalidas2@gmail.com>
2020-11-15 18:36:26 +0700

    update pwa feature

--- app/views/peserta/layouts/html_close.php
+++ app/views/peserta/layouts/html_close.php
@@ -1,12 +1,9 @@
-<script src="<?= base_url(); ?>/upup.min.js"></script>
-<script>
-   UpUp.start({
-      'content-url': '/public/offline.html', // show this when the user is offline
-      'assets': [ // define additional assets needed while offline:
-         '<?= base_url(); ?>public/assets/splash/images/logo.png', // such as images,
-         '<?= base_url(); ?>public/assets/splash/css/style.css', // custom stylesheets,
-      ]
-   });
+<script src="<?= base_url(); ?>pwabuilder-sw.js"></script>
+<script type="module">
+   import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+
+const el = document.createElement('pwa-update');
+document.body.appendChild(el);
 </script>
 <noscript>Browser tidak support javascript.</noscript>
 </body>
